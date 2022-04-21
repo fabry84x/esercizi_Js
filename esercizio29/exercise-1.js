a4144b829844281ed1a4243f72b8f61fd34e101c
Fabrizio <fabrizio.boschetto@email.it>
2022-04-21 19:25:59 +0200

    exercise 29 complete

---
+++ esercizio29/exercise-1.js
@@ -0,0 +1,27 @@
+function adultFilter(persons) {
+  // ...
+  let over18=[];
+  for(let i in persons){
+    if(persons[i].age>=18){
+      over18.push(persons[i]);
+    }
+  }
+  return over18;
+}
+
+const persons = [
+  { name: 'Paul', age: 16 },
+  { name: 'George', age: 17 },
+  { name: 'Lucas', age: 21 },
+  { name: 'Marco', age: 32 },
+  { name: 'Peter', age: 18 },
+  { name: 'Carl', age: 13 },
+  { name: 'Simon', age: 24 },
+  { name: 'Mark', age: 15 },
+  { name: 'Sandra', age: 34 },
+  { name: 'Alice', age: 28 }
+];
+
+const adults = adultFilter(persons);
+console.log(persons);
+console.log(adults);
