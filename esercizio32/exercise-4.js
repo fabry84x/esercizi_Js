0d2d364dda6f556d75d82625f9a3ce59a9927d57
Fabrizio <fabrizio.boschetto@email.it>
2022-04-22 12:41:11 +0200

    exercise 32 complete

---
+++ esercizio32/exercise-4.js
@@ -0,0 +1,64 @@
+function uncompletedNotes(notes) {
+  // ...
+  let uncomplete=[];
+  notes.forEach(element => {
+    if(element.todos.find(todo => todo.done===false)){
+      uncomplete.push(element);
+    }
+  });
+  return uncomplete;
+}
+
+const notes = [
+  {
+    id: 1,
+    description: 'Workout program',
+    todos: [
+      {
+        id: 1,
+        name: 'Push ups - 10 x 3',
+        done: false
+      },
+      {
+        id: 2,
+        name: 'Abdominals - 20 x 3',
+        done: true
+      },
+      {
+        id: 3,
+        name: 'Tapis Roulant - 15min',
+        done: true
+      }
+    ]
+  },
+  {
+    id: 2,
+    description: 'Front-end Roadmap',
+    todos: [
+      {
+        id: 1,
+        name: 'Learn HTML',
+        done: true
+      },
+      {
+        id: 2,
+        name: 'Learn CSS',
+        done: true
+      },
+      {
+        id: 3,
+        name: 'Learn JavaScript',
+        done: true
+      },
+      {
+        id: 4,
+        name: 'Learn Angular',
+        done: true
+      }
+    ]
+  }
+]
+
+const notesInProgress = uncompletedNotes(notes);
+console.log('All notes: ', notes);
+console.log('Notes In Progress: ', notesInProgress);
