4586d540c9ac6cccd4a3b22ad0929e44ac113867
Fabrizio <fabrizio.boschetto@email.it>
2022-05-16 17:20:20 +0200

    exercise 58 complete

---
+++ esercizio58/exercise-2.js
@@ -0,0 +1,34 @@
+const persons = [
+  {
+    id: 1,
+    firstName: 'Mario',
+    lastName: 'Rossi',
+    age: 25
+  },
+  {
+    id: 2,
+    firstName: 'Maria',
+    lastName: 'Verdi',
+    age: 32
+  },
+  {
+    id: 3,
+    firstName: 'Giovanni',
+    lastName: 'Rossi',
+    age: 35
+  }
+];
+
+function fetchPersonById(id) {
+  return new Promise((resolve,reject) => {
+    setTimeout(() => { 
+      if(id>0 && id<4) {
+        resolve(persons.find(item => item.id === id))
+      } else {
+      reject(new Error("id errato)"));
+        }
+    }, 1000);
+  });
+}
+
+fetchPersonById(4).then((person) => console.log(person));
