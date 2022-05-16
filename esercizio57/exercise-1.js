a6f0a3e27b1f9310250932e4a8a24ee1bf296bc2
Fabrizio <fabrizio.boschetto@email.it>
2022-05-16 13:21:35 +0200

    exercise 57 complete

---
+++ esercizio57/exercise-1.js
@@ -0,0 +1,46 @@
+const persons = [
+  {
+    id: 1,
+    firstName: 'Mario',
+    lastName: 'Rossi',
+    age: 25
+  },
+  {
+    id: 2,
+    firstName: 'Maria',
+    lastName: 'Verdi',
+    age: 32
+  },
+  {
+    id: 3,
+    firstName: 'Giovanni',
+    lastName: 'Rossi',
+    age: 35
+  }
+];
+
+function fetchPersonById(id) {
+  // code here
+  return new Promise((resolve,reject)=>{
+    if(id>0 && id<4){
+      resolve(id);
+    } else {
+    reject(new Error("id errato)"));
+    }
+  })
+    
+}
+// code here
+let promise = fetchPersonById(1);
+promise
+.then((id) => {
+  if(id===1){
+    console.log(persons[0]);
+  }
+  if(id===2){
+    console.log(persons[1]);
+  }
+  if(id===3){
+    console.log(persons[2]);
+  }
+})
